4a398dfc1837dd34787220f716c1ffce8784a5b8
Erta  Fekaj <erta.fekaj@umib.net>
2026-03-30 16:19:06 +0200

    🗑️ Remove UsersController and related DTOs, along with project configuration files

--- GlowBook.API/DTOs/UpdateUserDto.cs
+++
@@ -1,8 +0,0 @@
-namespace GlowBook.API.DTOs;
-
-public class UpdateUserDto
-{
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-}
