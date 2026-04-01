85931126c7459fed82b9dddb59bea105d87abc52
Erta  Fekaj <erta.fekaj@umib.net>
2026-04-01 22:53:56 +0200

    Update GlowBook.Application and GlowBook.Core assemblies with new builds and versioning
    - Updated GlowBook.Application and GlowBook.Core assemblies to reflect new builds.
    - Changed AssemblyInformationalVersion for both projects to "1.0.0+e26ab75d83cbcc072faaf93d420c690d46d2d7bf".
    - Updated project directory paths in editorconfig files to use consistent casing.
    - Regenerated assets and cache files for both projects.
    - Updated sourcelink JSON files to point to the latest commit on GitHub.
    - Ensured all binary files (DLLs, PDBs, and assets) are up to date in the Debug configuration.

--- GlowBook.Application/obj/Debug/net8.0/GlowBook.Application.AssemblyInfo.cs
+++ GlowBook.Application/obj/Debug/net8.0/GlowBook.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GlowBook.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c88ef3678bd892f5b9a839abea02f6d94330e1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26ab75d83cbcc072faaf93d420c690d46d2d7bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GlowBook.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GlowBook.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
